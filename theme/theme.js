f728a5a789f8c738cf7e3ad5e4babc8ec149ff38
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-01-30 19:48:36 +0530

    setup- theme

---
+++ theme/theme.js
@@ -0,0 +1,19 @@
+import { MD3LightTheme as DefaultTheme } from "react-native-paper";
+
+export default theme = {
+  ...DefaultTheme,
+  roundness: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#2588F6",
+    secondary: "#626AE2",
+    green: "#74D198",
+    pink: "#E7647E",
+    yellow: "#F9CE66",
+    orange: "#F39A71",
+  },
+  spacing: {
+    padding: 5,
+    margin: 5,
+  },
+};
