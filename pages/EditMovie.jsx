7feda43eed7e137c85272c9bc9a16abc8363b2cc
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-01-31 15:03:40 +0530

    Added Shared PageWrapper

--- pages/EditMovie.jsx
+++ pages/EditMovie.jsx
@@ -1,25 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
-import ROUTES from "./ROUTES";
+import { Text } from "react-native";
+import PageWrapper from "../components/Layout/PageWrapper";
 
 const EditMovie = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <PageWrapper>
       <Text>Edit Movie Screen</Text>
-      <Button onPress={() => navigation.navigate(ROUTES.HOME)} mode="outlined">
-        Go to Home
-      </Button>
-    </View>
+    </PageWrapper>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default EditMovie;
