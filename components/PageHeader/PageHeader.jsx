a492bf9f488bbd20e6e2215a6e0018ba62648833
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2023-02-08 23:02:00 +0530

    fixed: Page Header

---
+++ components/PageHeader/PageHeader.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Avatar, IconButton, Text } from "react-native-paper";
+
+const PageHeader = ({ setExitMenuOpen, exitMenuOpen }) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: 15,
+        paddingRight: 1,
+        marginVertical: 5,
+      }}
+    >
+      <Avatar.Icon size={40} icon="movie-open-check" />
+      <Text style={{ fontSize: 25, fontWeight: "bold" }}>Movies Collection</Text>
+      <IconButton
+        icon="dots-vertical"
+        size={30}
+        onPress={() => {
+          setExitMenuOpen(!exitMenuOpen);
+        }}
+      />
+    </View>
+  );
+};
+
+export default PageHeader;
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,12 @@
 import { QuerySnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
-import { useTheme } from "react-native-paper";
+import { Avatar, IconButton, Text, useTheme } from "react-native-paper";
 import DialogAlert from "../components/DialogAlert/DialogAlert";
+import ExitMenu from "../components/ExitMenu/ExitMenu";
 import PageWrapper from "../components/Layout/PageWrapper";
 import MovieCard from "../components/MovieCard/MovieCard";
+import PageHeader from "../components/PageHeader/PageHeader";
 import SearchBarComponent from "../components/SearchBar/SearchBar";
 import SnackBarComponent from "../components/SnackBar/SnackBar";
 import { auth, db, movieColRef } from "../core/config";
@@ -49,6 +51,8 @@ const Home = ({ navigation }) => {
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [searchQuery, setSearchQuery] = React.useState("");
 
+  const [exitMenuOpen, setExitMenuOpen] = useState(false);
+
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
       // The screen is focused
@@ -73,21 +77,30 @@ const Home = ({ navigation }) => {
   }, [searchQuery, movieData]);
 
   return (
-    <PageWrapper>
-      <SearchBarComponent
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
-      <ScrollView style={{ width: "100%" }}>
-        {filteredMovies.map((item, index) => (
-          <View key={index}>
-            <MovieCard navigation={navigation} cardData={item} />
-          </View>
-        ))}
-      </ScrollView>
-      <DialogAlert />
-      <SnackBarComponent />
-    </PageWrapper>
+    <>
+      {exitMenuOpen && (
+        <View style={{position:"absolute", top:110}}>
+          <ExitMenu navigation={navigation} />
+        </View>
+      )}
+      <PageWrapper>
+        <PageHeader exitMenuOpen={exitMenuOpen} setExitMenuOpen={setExitMenuOpen} />
+        <SearchBarComponent
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+
+        <ScrollView style={{ width: "100%" }}>
+          {filteredMovies.map((item, index) => (
+            <View key={index}>
+              <MovieCard navigation={navigation} cardData={item} />
+            </View>
+          ))}
+        </ScrollView>
+        <DialogAlert />
+        <SnackBarComponent />
+      </PageWrapper>
+    </>
   );
 };
 
