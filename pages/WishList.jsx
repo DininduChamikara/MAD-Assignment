ffc240d4405f93e45b9054c3d0908ce58a9bb852
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-02-10 23:42:31 +0530

    refactor code to use the custom hook

--- pages/WishList.jsx
+++ pages/WishList.jsx
@@ -1,50 +1,37 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import PageWrapper from "../components/Layout/PageWrapper";
 import SearchBarComponent from "../components/SearchBar/SearchBar";
 import SnackBarComponent from "../components/SnackBar/SnackBar";
 import WishlistCard from "../components/WishlistCard/WishlistCard";
+import { useMovies } from "../contexts/MovieProvider";
 import { auth } from "../core/config";
-import { Read } from "../core/databaseCrud";
 
 const WishList = ({ navigation }) => {
-  const [movieData, setMovieData] = useState([]);
-  const [filteredMovies, setFilteredMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const { movies, refreshMovies } = useMovies();
 
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
-      // The screen is focused
-      // Call any action
-      const respose = Read(auth ? auth.currentUser.uid : "");
-      respose
-        .then((data) => {
-          const filteredWishlist = data.filter(item => item.wishlistAdded === true);
-          setMovieData(filteredWishlist ? filteredWishlist : []);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      refreshMovies();
     });
     return unsubscribe;
   }, [navigation, auth.currentUser]);
 
-  useEffect(() => {
-    const filteredData = movieData.filter((item) =>
-      item.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredMovies(filteredData);
-  }, [searchQuery, movieData]);
-
   return (
     <PageWrapper>
-      <SearchBarComponent searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <SearchBarComponent
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
       <ScrollView style={{ width: "100%" }}>
-        {filteredMovies.map((item, index) => (
-          <View key={index}>
-            <WishlistCard navigation={navigation} cardData={item} />
-          </View>
-        ))}
+        {movies
+          .filter((item) => item.wishlistAdded === true)
+          .map((item, index) => (
+            <View key={index}>
+              <WishlistCard navigation={navigation} cardData={item} />
+            </View>
+          ))}
       </ScrollView>
       <SnackBarComponent />
     </PageWrapper>
