d4d0675ec2cb5e3a3e1dc88d8299ef7835c3a3fb
Gosala1999 <gosalaimeshjayaweera@gmail.com>
2023-02-11 09:22:21 +0530

    fix login changes

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,30 +1,41 @@
-import { Text } from "react-native";
-import { Button } from "react-native-paper";
-import PageWrapper from "../components/Layout/PageWrapper";
-import Main from "../components/Main/Main";
-import Register from "./Register";
-import ROUTES from "./ROUTES";
+import React from 'react'
+import { Text } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
+import PageWrapper from '../components/Layout/PageWrapper'
+import Main from '../components/Main/Main'
+import Register from './Register'
+import ROUTES from './ROUTES'
 
 const Login = ({ navigation }) => {
-  return (
-    <PageWrapper>
-      <Text>Login Screen</Text>
-      <Button
-        onPress={() => {
-          navigation.navigate(Main);
-        }}
-      >
-        Main
-      </Button>
-      <Button
-        onPress={() => {
-          navigation.navigate(Register);
-        }}
-      >
-        Register
-      </Button>
-    </PageWrapper>
-  );
-};
+	const [text, setText] = React.useState('')
 
-export default Login;
+	return (
+		<PageWrapper>
+			<Text>Login Screen</Text>
+			<TextInput
+				label='Email'
+				value={text}
+				onChangeText={(text) => setText(text)}
+			/>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+			<Button
+				onPress={() => {
+					navigation.navigate(Main)
+				}}
+			>
+				Main
+			</Button>
+			<Button
+				onPress={() => {
+					navigation.navigate(Register)
+				}}
+			>
+				Register
+			</Button>
+		</PageWrapper>
+	)
+}
+
+export default Login
